0b5ac5c255cd6ed6743f3679a5b844208d8233cb
balazs4 <balazs4web@gmail.com>
2017-01-15 16:37:42 +0100

    Add an ugly but mobile-ready index.js

--- pages/index.js
+++ pages/index.js
@@ -1,1 +1,27 @@
-export default () => <div>"Hello World"</div>
+import React from 'react';
+import Head from 'next/head';
+
+
+const Channel = ({name = 'Radio', click}) => (
+	<div>
+		<p>{name}</p>
+		<button onClick={e => click()}>Listen</button>
+	</div>
+)
+
+const ChannelList = ({channels}) => (
+	<div id="radio-list">
+		<ul> { channels.map(ch => <li><Channel /></li>) } </ul>
+	</div>
+)
+
+
+export default () => (
+	<div>
+		<Head>
+			<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+			<title>radio-client</title>
+		</Head>
+		<ChannelList channels={['1', '2','3', '4']}/>
+	</div>
+)
